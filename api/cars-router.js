2d67860a0e42ad88a66ee5c5c063de7fdc26c757
Carl-Stanley <cds.beml@gmail.com>
2019-11-16 10:39:26 -0700

    Commit

---
+++ api/cars-router.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const knex = require('knex');
+
+const db = knex({
+  client: 'sqlite3',
+  connection: {
+    filename: './data/car-dealer.db'
+  },
+  useNullAsDefault: true
+});
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  db('cars')
+  .then(fruits => {
+    res.json(cars); 
+  })
+  .catch (err => {
+    res.status(500).json({ message: 'Failed to retrieve cars' });
+  });
+});
