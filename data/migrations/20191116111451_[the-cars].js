539d2ef54f741027bd6f0a237d5b262a53c9bc2f
Carl-Stanley <cds.beml@gmail.com>
2019-11-16 11:50:14 -0700

    Commit

---
+++ data/migrations/20191116111451_[the-cars].js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex, Promise) {
+    // don't forget the return statement
+    return knex.schema.createTable('cars', tbl => {
+      // creates a primary key called id
+      tbl.increments();
+      // creates a text field called name which is both required and unique
+      tbl.text('vin', 128).unique().notNullable();
+      tbl.text('make', 128).notNullable();
+      tbl.text('model', 128).notNullable();
+      tbl.integer('mileage').notNullable();
+      tbl.text('transmission_type', 128).Nullable();
+      tbl.text('title_status', 128).Nullable();
+    });
+  };
+  
+  exports.down = function(knex, Promise) {
+    // drops the entire table
+    return knex.schema.dropTableIfExists('cars');
+  };
