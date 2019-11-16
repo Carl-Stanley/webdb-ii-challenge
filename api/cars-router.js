3fdb2845c7b7b8b84522f93f64cf836603f6d3e1
Carl-Stanley <cds.beml@gmail.com>
2019-11-16 11:01:00 -0700

    Commit

--- api/cars-router.js
+++ api/cars-router.js
@@ -11,6 +11,7 @@ const db = knex({
 
 const router = express.Router();
 
+// All Cars
 router.get('/', (req, res) => {
   db('cars')
   .then(fruits => {
@@ -20,5 +21,35 @@ router.get('/', (req, res) => {
     res.status(500).json({ message: 'Failed to retrieve cars' });
   });
 });
+
+// One car
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+  
+    db('cars').where({ id }).first()
+    .then(car => {
+      res.json(car);
+    }) 
+    .catch (err => {
+      res.status(500).json({ message: 'Failed to retrieve car' });
+    });
+  });
+  
+  // Add a car
+  router.post('/', (req, res) => {
+    const carData = req.body;
+    db('cars').insert(carData)
+    .then(ids => {
+      db('cars').where({ id: ids[0] })
+      .then(newCarEntry => {
+        res.status(201).json(newCarEntry);
+      });
+    })
+    .catch (err => {
+      console.log('POST error', err);
+      res.status(500).json({ message: "Failed to store data" });
+    });
+  });
+  
 
 module.exports = router;
